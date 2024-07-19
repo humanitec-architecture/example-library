515e09c090d6e17087805c7369aea4baea71b1f9
Mathieu Benoit <mathieu.benoit@humanitec.com>
2024-07-19 18:09:50 +0000

    values_string

--- resource-definitions/terraform-driver/s3/public-git-repo.tf
+++ resource-definitions/terraform-driver/s3/public-git-repo.tf
@@ -13,24 +13,20 @@ resource "humanitec_resource_definition" "aws_terraform_resource_s3_bucket" {
       })
     }
 
-    values = {
+    values_string = jsonencode({
       # Connection information to the Git repo containing the Terraform code
       # The repo must not require authentication
-      "source" = jsonencode(
-        {
+      source = {
           path = "s3/terraform/bucket/"
           rev  = "refs/heads/main"
           url  = "https://my-domain.com/my-org/my-repo.git"
-        }
-      )
-      "variables" = jsonencode(
-        {
+      }
+      variables = {
           # Provide a separate bucket per Application and Environment
           bucket          = "my-company-my-app-$${context.app.id}-$${context.env.id}"
           region          = var.region
           assume_role_arn = var.assume_role_arn
-        }
-      )
-    }
+      }
+    })
   }
 }
