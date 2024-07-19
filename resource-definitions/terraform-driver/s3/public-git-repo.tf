9ea8134154004b635368df72bd0abc2b29787482
Mathieu Benoit <mathieu.benoit@humanitec.com>
2024-07-19 18:15:36 +0000

    terraform fmt

--- resource-definitions/terraform-driver/s3/public-git-repo.tf
+++ resource-definitions/terraform-driver/s3/public-git-repo.tf
@@ -17,15 +17,15 @@ resource "humanitec_resource_definition" "aws_terraform_resource_s3_bucket" {
       # Connection information to the Git repo containing the Terraform code
       # The repo must not require authentication
       source = {
-          path = "s3/terraform/bucket/"
-          rev  = "refs/heads/main"
-          url  = "https://my-domain.com/my-org/my-repo.git"
+        path = "s3/terraform/bucket/"
+        rev  = "refs/heads/main"
+        url  = "https://my-domain.com/my-org/my-repo.git"
       }
       variables = {
-          # Provide a separate bucket per Application and Environment
-          bucket          = "my-company-my-app-$${context.app.id}-$${context.env.id}"
-          region          = var.region
-          assume_role_arn = var.assume_role_arn
+        # Provide a separate bucket per Application and Environment
+        bucket          = "my-company-my-app-$${context.app.id}-$${context.env.id}"
+        region          = var.region
+        assume_role_arn = var.assume_role_arn
       }
     })
   }
