c280d79c642e67c37321dcb005f95d66c198e6b2
Tobias Babin <tobias.babin@humanitec.com>
2024-07-30 11:40:42 +0200

    Testing formatting correction for auto generation

--- resource-definitions/k8s-cluster-eks/agent/eks-agent.tf
+++ resource-definitions/k8s-cluster-eks/agent/eks-agent.tf
@@ -6,7 +6,7 @@ resource "humanitec_resource_definition" "eks-agent" {
   driver_account = "aws-temp-creds"
   driver_inputs = {
     values_string = jsonencode({
-      "region"                   = "eu-central-1"
+      "region"                 = "eu-central-1"
       "name"                     = "demo-123"
       "loadbalancer"             = "x111111xxx111111111x1xx1x111111x-x111x1x11xx111x1.elb.eu-central-1.amazonaws.com"
       "loadbalancer_hosted_zone" = "ABC0DEF5WYYZ00"
