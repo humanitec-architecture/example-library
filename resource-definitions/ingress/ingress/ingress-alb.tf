1fb01a1c70968375c241c18f27fa1452d9db3a17
Bobby DeVeaux <bobby.deveaux@humanitec.com>
2024-08-02 13:52:52 +0100

    Removing double jsonencode

--- resource-definitions/ingress/ingress/ingress-alb.tf
+++ resource-definitions/ingress/ingress/ingress-alb.tf
@@ -12,14 +12,14 @@ resource "humanitec_resource_definition" "ingress" {
 
   driver_inputs = {
     values_string = jsonencode({
-      "annotations" : jsonencode({
+      "annotations" : {
         "alb.ingress.kubernetes.io/certificate-arn" : "${var.ingress_cert_arn}",
         "alb.ingress.kubernetes.io/group.name" : "${var.ingress_group_name}",
         "alb.ingress.kubernetes.io/listen-ports" : "[{\"HTTP\":80},{\"HTTPS\":443}]",
         "alb.ingress.kubernetes.io/scheme" : "internet-facing",
         "alb.ingress.kubernetes.io/ssl-redirect" : "443",
         "alb.ingress.kubernetes.io/target-type" : "ip"
-      }),
+      },
       "class" : "alb",
       "no_tls" : true
     })
