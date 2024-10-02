e18b0fa6b327f3926224e398ae9c843376b4febf
Tobias Babin <tobias.babin@humanitec.com>
2024-10-02 08:14:43 +0200

    Adjusted string formatting for YAML

--- resource-definitions/wildcard-dns/dns-template.tf
+++ resource-definitions/wildcard-dns/dns-template.tf
@@ -5,9 +5,19 @@ resource "humanitec_resource_definition" "dns-template" {
   type        = "dns"
   driver_inputs = {
     values_string = jsonencode({
-      "domain"   = "my-test-domain.com"
-      "template" = "{{ index (splitList "." "$${context.res.id}") 1 }}-$${context.env.id}-$${context.app.id}"
+      "domain"   = "my-domain.com"
+      "template" = "{{ index (splitList \".\" \"$${context.res.id}\") 1 }}-$${context.env.id}-$${context.app.id}"
     })
+  }
+
+  provision = {
+    "ingress" = {
+      is_dependent = false
+    }
   }
 }
 
+resource "humanitec_resource_definition_criteria" "dns-template_criteria_0" {
+  resource_definition_id = resource.humanitec_resource_definition.dns-template.id
+
+}
