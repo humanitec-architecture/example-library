b80c93523618577346935137d9d0a9e5724af726
Bianca Del Carretto <delca85@users.noreply.github.com>
2024-10-17 16:23:13 +0200

    feat(k8s-cluster-git): provide example to fetch runtime from additional cluster

---
+++ resource-definitions/k8s-cluster-git/runtime/gke-temporary-credentials-runtime.tf
@@ -0,0 +1,20 @@
+resource "humanitec_resource_definition" "gke-temporary-credentials-runtime" {
+  driver_type    = "humanitec/k8s-cluster-gke"
+  id             = "gke-temporary-credentials-runtime"
+  name           = "gke-temporary-credentials-runtime"
+  type           = "k8s-cluster"
+  driver_account = "gcp-temporary-creds"
+  driver_inputs = {
+    values_string = jsonencode({
+      "loadbalancer" = "35.10.10.10"
+      "name"         = "demo-123"
+      "zone"         = "europe-west2-a"
+      "project_id"   = "my-gcp-project"
+    })
+  }
+}
+
+resource "humanitec_resource_definition_criteria" "gke-temporary-credentials-runtime_criteria_0" {
+  resource_definition_id = resource.humanitec_resource_definition.gke-temporary-credentials-runtime.id
+  res_id                 = "k8s-cluster-runtime"
+}
